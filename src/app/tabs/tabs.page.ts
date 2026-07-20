1c98a11c44c6029e787bf59b0c8d823d3854fb47
LeilaniSerna <leilanisernagomez81@gmail.com>
2026-07-20 14:48:04 -0600

    cambio de distribucion

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,13 +1,23 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { hardwareChipOutline, settingsOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
+  styleUrls: ['tabs.page.scss'],
   standalone: true,
   imports: [IonicModule],
 })
 export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
-  constructor() {}
+
+  constructor() {
+    // Registrar íconos oficiales para la barra de pestañas
+    addIcons({
+      hardwareChipOutline,
+      settingsOutline
+    });
+  }
 }
